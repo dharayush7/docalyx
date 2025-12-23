2303fab5d3cc64fd8eb18b7cbac62e5d98b23122
AYUSH DHAR <contact@ayushdhar.com>
2025-12-23 10:35:10 +0530

    style(auth): replace sparkles icon with docalyx logo in auth card header

--- client/src/app/auth/main.tsx
+++ client/src/app/auth/main.tsx
@@ -7,16 +7,24 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { LogIn, Sparkles, UserPlus } from "lucide-react";
+import { LogIn, UserPlus } from "lucide-react";
 import { useRouter } from "next/navigation";
+import logo from "@/assets/docalyx.png";
+import Image from "next/image";
 
 export default function Main() {
   const router = useRouter();
   return (
     <Card className="w-full max-w-md shadow-lg">
       <CardHeader className="space-y-3 text-center">
-        <div className="mx-auto w-14 h-14 bg-primary rounded-xl flex items-center justify-center mb-2">
-          <Sparkles className="w-7 h-7 text-primary-foreground" />
+        <div className="mx-auto w-14 h-14 rounded-xl flex items-center justify-center mb-2">
+          <Image
+            src={logo}
+            alt="Logo"
+            className="w-fit h-full"
+            width={100}
+            height={100}
+          />
         </div>
         <CardTitle className="text-2xl">Welcome Back</CardTitle>
         <CardDescription>
