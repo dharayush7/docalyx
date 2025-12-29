2016fa0b56c4952422b6c98d517d747f2ccb867c
AYUSH DHAR <contact@ayushdhar.com>
2025-12-29 10:17:38 +0530

    style(ai-chat-card): remove unused highlight.js css import and fix formatting

--- client/src/components/ai-chat-card.tsx
+++ client/src/components/ai-chat-card.tsx
@@ -2,7 +2,6 @@ import { messages } from "@/generated/prisma/client";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import rehypeHighlight from "rehype-highlight";
-import "highlight.js/styles/github.css";
 import CodeBlock from "./code-block";
 import {
   cn,
@@ -106,7 +105,7 @@ export default function AiChatCard({ message }: { message: messages }) {
           onClick={handleCopy}
           className={cn(
             "opacity-0 group-hover/chat:opacity-100 transition text-secondary-foreground rounded flex items-center justify-center cursor-pointer",
-            isMobile && "opacity-100",
+            isMobile && "opacity-100"
           )}
         >
           {copied ? <Check size={16} /> : <Copy size={16} />}
